b386e332444401c88d29df49deb38d56713f0e99
fernandabelenVL <fernanda.vegaleiva@gmail.com>
2019-07-08 12:06:57 -0400

    funcion para crear cartas, ordenar funcionando, filtrar funcionando

--- src/data.js
+++ src/data.js
@@ -1,73 +1,23 @@
-/*const sortData = (data, sortBy, condition) => {
-  let arr = [];
-
-  if (sortBy == "name"){
-
-    if(condition === "nameUp"){
-     
-      arr = data.sort((a, b) => a.name.localeCompare(b.name));
-    }
-    else {
-      arr = data.sort((a, b) => a.name.localeCompare(b.name)).reverse();
-    }
-  }
-  else {
-    if(condition === "numUp"){
-      arr = data.sort(sortById);
-    }
-    else {
-      arr = data.sort(sortById).reverse();
-    }
-  }
-  return arr;
-}*/
-
-/* Manejo de data */
-
 let data = POKEMON.pokemon;
+
 //ordenar de la A a la Z
 function nameUp (){
 data.sort(function(a, b){
   return a.name.localeCompare(b.name);
 }); 
 };
-
 //ordenar de la Z a la A
 function nameDown (){
 data.sort(function(a, b){
 	return b.name.localeCompare(a.name);
 });
 };
-
-
-function filterPoke() {
-  document.getElementById("demo").innerHTML = data.stringify(data.filter(TypeId));
-}
-
-
-
-//ordenar de por ID
-/*function sortbyId () {
-data.sort(function(a,b){
-  if (a.id > b.id) {
-    return 1;
-  } 
-  else if (b.id > a.id) {
-    return -1;
-  } 
-  else {
-    return 0;
-  }
-})
-}*/
-
 //ordenar numero ascendente
 function numUp (){
   data.sort(function(a, b){
     return a.id - b.id
   });
 }
-
 //ordenar numero ascendente
 function numDown (){
   data.sort(function(a, b){
@@ -75,12 +25,17 @@ function numDown (){
   });
 }
 
-
-//filtrar por tipo
-/*const filterPokeType = (data, condition)=>{
-  const resultFilter = data.filter(element =>{
-    return element.type.includes(condition);
-  }) 
-  return resultFilter;
+//Filtrar por Tipo
+const filterTypesPokemon = (type) => {
+data = POKEMON.pokemon;
+let filter = [];
+  for (i = 0; i < data.length; i++) {
+      if (data[i].type[0] == type) {
+          filter.push(data[i]);
+      } else if (data[i].type[1] == type) {
+          filter.push(data[i]);
+      }
+  }
+return filter;
 }
-window.filterPokeType=filterPokeType;*/
+window.filterTypesPokemon = filterTypesPokemon;
